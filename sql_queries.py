9e32098e7e60a16b9f800016677260f89f7055f9
Your Name <abdelwahab.chetara@g.enp.edu.dz>
2025-04-22 16:01:47 -0400

    create sql queries

--- sql_queries.py
+++ sql_queries.py
@@ -122,7 +122,7 @@ COPY staging_events
 FROM '{LOG_DATA}'
 IAM_ROLE '{IAM_ROLE}'
 JSON '{LOG_JSONPATH}'
-REGION 'us-west-2';
+
 """
 
 staging_songs_copy = f"""
@@ -130,7 +130,7 @@ COPY staging_songs
 FROM '{SONG_DATA}'
 IAM_ROLE '{IAM_ROLE}'
 JSON 'auto'
-REGION 'us-west-2';
+
 """
 
 # INSERT INTO FINAL TABLES
@@ -148,4 +148,67 @@ SELECT DISTINCT
     se.location,
     se.userAgent
 FROM staging_events se
-JOIN
+JOIN staging_songs ss
+  ON se.song = ss.title AND se.artist = ss.artist_name AND se.length = ss.duration
+WHERE se.page = 'NextSong'
+AND se.userId IS NOT NULL;
+""")
+
+user_table_insert = ("""
+INSERT INTO users (user_id, first_name, last_name, gender, level)
+SELECT DISTINCT userId, firstName, lastName, gender, level
+FROM staging_events
+WHERE userId IS NOT NULL;
+""")
+
+song_table_insert = ("""
+INSERT INTO songs (song_id, title, artist_id, year, duration)
+SELECT DISTINCT song_id, title, artist_id, year, duration
+FROM staging_songs
+WHERE song_id IS NOT NULL;
+""")
+
+artist_table_insert = ("""
+INSERT INTO artists (artist_id, name, location, latitude, longitude)
+SELECT DISTINCT artist_id, artist_name, artist_location, artist_latitude, artist_longitude
+FROM staging_songs
+WHERE artist_id IS NOT NULL;
+""")
+
+time_table_insert = ("""
+INSERT INTO time (start_time, hour, day, week, month, year, weekday)
+SELECT DISTINCT
+    start_time,
+    EXTRACT(hour FROM start_time),
+    EXTRACT(day FROM start_time),
+    EXTRACT(week FROM start_time),
+    EXTRACT(month FROM start_time),
+    EXTRACT(year FROM start_time),
+    EXTRACT(weekday FROM start_time)
+FROM (
+    SELECT TIMESTAMP 'epoch' + ts/1000 * INTERVAL '1 second' AS start_time
+    FROM staging_events
+    WHERE page = 'NextSong'
+);
+""")
+
+# QUERY LISTS
+create_table_queries = [
+    staging_events_table_create,
+    staging_songs_table_create,
+    user_table_create,
+    artist_table_create,
+    song_table_create,
+    time_table_create,
+    songplay_table_create
+]
+
+drop_table_queries = [
+    staging_events_table_drop,
+    staging_songs_table_drop,
+    songplay_table_drop,
+    user_table_drop,
+    song_table_drop,
+    artist_table_drop,
+    time_table_drop
+]
